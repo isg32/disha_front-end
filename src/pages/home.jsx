cb60ee1383409a1cff96cb9c373b383544ba19e2
Sapan Gajjar <sapangajjar101105@gmail.com>
2024-12-29 16:51:48 +0530

    [temp] ui: use static image with relative path instead of slide show since vercel does not allow unregistered paths
    Signed-off-by: Sapan Gajjar <sapangajjar101105@gmail.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,12 +3,14 @@ import Navbar from '../Static-components/navbar.jsx'
 import Footer from '../Static-components/footer.jsx'
 import '../styles/style.css'
 import Slideshow from '../Static-components/slideshow.jsx'
+import slideshowplaceholder from '../assets/images/slideshow/image 34.png'
 
 function HomePage() {
   return (
     <div className="home-page">
       <Navbar/>
-      <Slideshow/>
+      <img src={slideshowplaceholder} alt="Slideshow placeholder" style={{width: 'auto', height: '100%'}} />
+      {/*<Slideshow/>*/}
       <Footer/>
     </div>
   )
