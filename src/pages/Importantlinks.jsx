71b7723508532f99b2904a3deae1f0100295f10c
Sapan Gajjar <sapangajjar101105@gmail.com>
2024-12-30 21:51:35 +0530

    page: completed important link's page
    Signed-off-by: Sapan Gajjar <sapangajjar101105@gmail.com>

---
+++ src/pages/Importantlinks.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import Navbar from '../Static-components/navbar.jsx'
+import Footer from '../Static-components/footer.jsx'
+import '../styles/style.css'
+import Importantlinksbannerplaceholder from '../assets/images/generic-placeholder-banner.png'
+import '../styles/importantlinks.css'
+import awomanholdingpapers from '../assets/images/chicktemplate.png'
+import ourpartners from '../assets/images/ourpartneres.png'
+
+function Importantlinks() {
+  return (
+    <div className="home-page">
+
+      {/* Navbar */}
+      <Navbar/> 
+      
+      {/* Slideshow */}
+      {/***
+       * The slideshow is not commonized into a seperate module
+       * since it is not used in other pages
+       * and it is used as base for two overlays [sandwitch]
+       * **/}
+      <img src={Importantlinksbannerplaceholder} alt="custom page placeholder" style={{width: '100%'}} />
+
+    {/* 2 sections - important links with button/details and a chick  */}
+    <div className='importantlinks-main'>
+        <div className='side-left'>
+            <h1>Important Links</h1>
+            <h2>Detail 1</h2>
+            <ul>
+                <li>Nature's Wonders - Capture the beauty of nature in your surroundings.</li>
+                <li>Life in Motion - Showcase moments that express movement and energy.</li>
+                <li>Colors of Culture - Highlight diversity and vibrancy within your community.</li>
+            </ul>
+            <div className='DivineButtons'>
+                <button className='hvr-grow' style={{margin: '10px', border: 'none' ,borderRadius: '50px' ,background: '#FF9A00',boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}} >Download Previous Year Papers</button>
+                <button className='hvr-grow' style={{margin: '10px', border: 'none' ,borderRadius: '50px' ,background: '#FF9A00',boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}} >See Exam results</button>
+                <button className='hvr-grow' style={{margin: '10px', border: 'none' ,borderRadius: '50px' ,background: '#FF9A00',boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}} >Check Exam Dates</button>
+                <button className='hvr-grow' style={{margin: '10px', border: 'none' ,borderRadius: '50px' ,background: '#FF9A00',boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}} >Download Admit Card</button>
+                <button className='hvr-grow' style={{margin: '10px', border: 'none' ,borderRadius: '50px' ,background: '#FF9A00',boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}} >See Last Examination's Rans</button>
+            </div>
+        </div>
+
+        <div className='side-right'>
+        <img src={awomanholdingpapers} alt="" />
+        </div>
+    </div>
+
+    {/* yellow lable */}
+    <div className='learnmoresplit' style={{justifyContent: 'center', padding:'2%'}}>
+        <div className='button-out hvr-grow'>
+          <button className='buttonsi'>
+          <p>Register Your school</p>
+          </button>
+        </div>
+      </div>
+
+    {/* The Sponcers' table */}
+    <div>
+        <img src={ourpartners} alt="our partneres" 
+            style={{
+                width: '90%',
+                margin: '10px'
+            }}
+        />
+    </div>
+
+    {/* Footer */}
+      <Footer/>
+    </div>
+  )
+}
+
+export default Importantlinks
